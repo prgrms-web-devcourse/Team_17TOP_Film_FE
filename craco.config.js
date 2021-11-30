8c904980896c0e3d1192d102c645f251aaf838f0
이소정 <2sjeong918@gmail.com>
2021-12-01 01:14:17 +0900

    :wrench:  config: 개발환경설정

---
+++ craco.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  babel: {
+    presets: [
+      '@emotion/babel-preset-css-prop',
+      '@babel/preset-env',
+      '@babel/preset-react',
+      '@babel/preset-typescript',
+    ],
+  },
+};
