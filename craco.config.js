513a722f6a5ccb85522bf4a7e08782eb014791d2
이소정 <2sjeong918@gmail.com>
2021-12-01 02:08:50 +0900

    :bug:  fix: runtime환경에서 react is not defined 에러 수정
    컴파일에서는 문제 없었는데, 런타임 시 에러발생

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   babel: {
-    presets: [
-      '@emotion/babel-preset-css-prop',
-      '@babel/preset-env',
-      '@babel/preset-react',
-      '@babel/preset-typescript',
-    ],
+    presets: ['@emotion/babel-preset-css-prop'],
   },
 };
