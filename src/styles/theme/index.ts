0882098d0f55044f16359d453fdcecdedd28f73f
jinn2u <bsybear623@gmail.com>
2021-12-04 15:11:36 +0900

    :lipstick:  style: 프로젝트에 사용될 색상변수 정의

--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -1,10 +1,10 @@
-import font from './fonts';
-
+import fonts from './fonts';
+import colors from './colors';
 export type ThemeType = typeof theme;
 declare module '@emotion/react' {
   export interface Theme extends ThemeType {}
 }
-
 export const theme = {
-  font,
+  fonts,
+  colors,
 } as const;
