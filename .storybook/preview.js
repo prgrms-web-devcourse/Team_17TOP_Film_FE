34003683bd273d1c33e518866883f68b3e12e949
이소정 <2sjeong918@gmail.com>
2021-12-07 16:14:27 +0900

    :art:  style: storybook에 emotion theme, reset 스타일 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,24 @@
+import { ThemeProvider } from 'emotion-theming';
+import { theme } from '../src/styles/theme';
+import reset from '../src/styles/reset';
+import { addDecorator } from '@storybook/react';
+import { Global } from '@emotion/react';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+const EmotionThemeProvider = (storyFn) => (
+  <ThemeProvider theme={theme}>
+    <Global styles={reset} />
+    {storyFn()}
+  </ThemeProvider>
+);
+
+addDecorator(EmotionThemeProvider);
