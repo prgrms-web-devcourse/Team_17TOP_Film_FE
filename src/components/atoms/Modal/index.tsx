a5ca1e3f936e0f9210cc60ea9ae5988a4ff73f47
jinn2u <bsybear623@gmail.com>
2021-12-07 16:19:54 +0900

    :sparkles:  feature: 모달이 떳을 경우 스크롤 이벤트 막기

--- src/components/atoms/Modal/index.tsx
+++ src/components/atoms/Modal/index.tsx
@@ -7,9 +7,25 @@ interface Props {
   visible: boolean;
   onClose: (value: boolean) => void;
 }
-const Modal = ({ children, visible, onClose }: Props) => {
+const Modal = ({ children, visible }: Props) => {
   //
+  const preventScroll = useCallback((e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+  }, []);
 
+  useEffect(() => {
+    if (!visible) return;
+    window.addEventListener('scroll', preventScroll, { capture: false, passive: false });
+    window.addEventListener('touchmove', preventScroll, { capture: false, passive: false });
+    window.addEventListener('mousewheel', preventScroll, { capture: false, passive: false });
+    return () => {
+      window.removeEventListener('scroll', preventScroll);
+      window.removeEventListener('touchmove', preventScroll);
+      window.removeEventListener('mousewheel', preventScroll);
+    };
+  }, [visible]);
   return ReactDOM.createPortal(
     <BlackBG visible={visible} tabIndex={-1}>
       <Wrapper>{children}</Wrapper>
