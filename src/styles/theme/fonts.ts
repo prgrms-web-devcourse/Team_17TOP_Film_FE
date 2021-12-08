e5edec4367f224ae2f58b87c207d16d68300cd80
이소정 <2sjeong918@gmail.com>
2021-12-09 02:50:59 +0900

    :lipstick:  style: theme font 기본컬러 추가, Heading line-height 삭제

--- src/styles/theme/fonts.ts
+++ src/styles/theme/fonts.ts
@@ -1,38 +1,43 @@
+import colors from './colors';
+
 const font = {
   Heading1: {
     fontSize: '3rem',
     fontWeight: 700,
-    lineHeight: 1.4,
+    color: colors.gray900,
   },
   Heading2: {
     fontSize: '2.250rem',
     fontWeight: 700,
-    lineHeight: 1.4,
+    color: colors.gray900,
   },
   Heading3: {
     fontSize: '1.500rem',
     fontWeight: 700,
-    lineHeight: 1.4,
+    color: colors.gray900,
   },
   Heading4: {
-    fontSize: '1.250rem',
+    fontSize: '1.125rem',
     fontWeight: 700,
-    lineHeight: 1.4,
+    color: colors.gray800,
   },
   Paragraph1: {
     fontSize: '1rem',
     fontWeight: 400,
     lineHeight: 1.4,
+    color: colors.gray700,
   },
   Paragraph2: {
     fontSize: '0.875rem',
     fontWeight: 400,
     lineHeight: 1.4,
+    color: colors.gray700,
   },
   smallText: {
     fontSize: '0.750rem',
     fontWeight: 400,
     lineHeight: 1.4,
+    color: colors.gray600,
   },
 };
 export default font;
