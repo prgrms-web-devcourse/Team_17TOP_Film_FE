eb04e4620be142ba3ed44069d3c2b2613976f99a
이소정 <2sjeong918@gmail.com>
2021-12-09 02:53:05 +0900

    :sparkles:  feat: 엿보기 컴포넌트 더미데이터로 마크업, 스타일링

--- src/components/organism/PreviewBottomSheet/index.tsx
+++ src/components/organism/PreviewBottomSheet/index.tsx
@@ -1,8 +1,59 @@
-import { useParams } from 'react-router';
+import { useParams, Link } from 'react-router-dom';
+import { Button, Text } from '../../atoms';
+import {
+  BottomSheetWrapper,
+  FilmTitle,
+  FilmPreviewText,
+  DateWrapper,
+  FilmOpenDate,
+  CloseBtn,
+} from './style';
 
+const dummyPost = {
+  postId: 0,
+  title: '제목입니당',
+  previewText: '엿보기 문구입니당',
+  availableAt: 'yyyy-MM-dd',
+  state: 'Closed',
+  location: [
+    {
+      latitude: '37.491837217869616',
+      longitude: '127.02959879978368',
+    },
+  ],
+  authorityCount: 3,
+  authorityImageList: [
+    {
+      imageOrder: 0,
+      authorityId: 0,
+      imageUrl: '',
+    },
+    {
+      imageOrder: 0,
+      authorityId: 0,
+      imageUrl: '',
+    },
+  ],
+};
 const PreviewBottomSheet = () => {
   const { id } = useParams();
-  console.log('params :>> ', id);
-  return <div>PreviewBottomSheet</div>;
+  return (
+    <BottomSheetWrapper>
+      <FilmTitle textType="Heading3">{dummyPost.title}</FilmTitle>
+      <FilmPreviewText textType="Paragraph1">
+        #{id}필름의 {dummyPost.previewText}
+      </FilmPreviewText>
+      <DateWrapper>
+        <Text textType="Heading4">사진 나오는 날</Text>
+        <FilmOpenDate textType="Paragraph2">{dummyPost.availableAt}</FilmOpenDate>
+      </DateWrapper>
+      <Button buttonType="PrimaryBtn" width="100%">
+        필름 삭제하기
+      </Button>
+      <CloseBtn>
+        <Link to="/">닫기</Link>
+      </CloseBtn>
+    </BottomSheetWrapper>
+  );
 };
 export default PreviewBottomSheet;
---
+++ src/components/organism/PreviewBottomSheet/style.ts
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+import { Text } from '../../atoms';
+
+const BottomSheetWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 20px;
+  background-color: #fff;
+  border-top: 1px solid ${({ theme }) => theme.colors.gray300};
+`;
+const FilmTitle = styled(Text)``;
+const FilmPreviewText = styled(Text)`
+  margin-top: ${({ theme }) => theme.gaps.gap2};
+  color: ${({ theme }) => theme.colors.gray700};
+`;
+const DateWrapper = styled.div`
+  padding: ${({ theme }) => theme.gaps.gap2} 0;
+`;
+const FilmOpenDate = styled(Text)`
+  margin-top: ${({ theme }) => theme.gaps.gap1};
+`;
+const CloseBtn = styled.button`
+  position: absolute;
+  top: ${({ theme }) => theme.gaps.default_margin};
+  right: ${({ theme }) => theme.gaps.default_margin};
+`;
+
+export { BottomSheetWrapper, FilmTitle, FilmPreviewText, DateWrapper, FilmOpenDate, CloseBtn };
