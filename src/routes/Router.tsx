0965386adb248af10ca17e595be6f6b845975605
이소정 <2sjeong918@gmail.com>
2021-12-09 18:14:06 +0900

    :bug:  fix: 라우트 경로 수정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -5,8 +5,8 @@ const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="login" element={<LogInPage />} />
-      <Route path="post/create" element={<CreatePostPage />} />
+      <Route path="/login" element={<LogInPage />} />
+      <Route path="/post/create" element={<CreatePostPage />} />
     </Routes>
   );
 };
