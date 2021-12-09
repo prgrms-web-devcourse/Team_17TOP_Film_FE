90c18be0becb616cc01fc8705a1cfd80ec99e98b
jinn2u <bsybear623@gmail.com>
2021-12-09 11:32:00 +0900

    :sparkles:  feat: AvatarGroup컴포넌트 생성

--- src/components/atoms/Avatar/AvatarGroup.tsx
+++ src/components/atoms/Avatar/AvatarGroup.tsx
@@ -1,17 +1,33 @@
-import React, { ReactNode } from 'react';
+import React, { ReactElement, ReactNode } from 'react';
+import styled from '@emotion/styled';
 import { VALID_AVATAR } from './constants';
 
 interface Props {
   children: ReactNode;
+  overlapPx?: number;
 }
 
-const AvatarGroup = ({ children }: Props) => {
-  const avatars = React.Children.toArray(children).filter((element: ReactNode) => {
-    if (React.isValidElement(element) && element.props.__TYPE === VALID_AVATAR) {
-      return true;
-    }
-    return false;
-  });
-  return <div>{children}</div>;
+const AvatarGroup = ({ children, overlapPx = 0 }: Props) => {
+  const avatars = React.Children.toArray(children)
+    .filter((element: ReactNode) => {
+      if (React.isValidElement(element) && element.props.__TYPE === VALID_AVATAR) {
+        return true;
+      }
+      return false;
+    })
+    .map((avatar, index, avatars) => {
+      const item = avatar as ReactElement;
+      return React.cloneElement(item, {
+        ...item.props,
+        style: {
+          marginLeft: `${index === 0 ? 0 : -overlapPx}px`,
+          zIndex: avatars.length - index,
+        },
+      });
+    });
+  return <div>{avatars}</div>;
 };
+const Div = styled.div`
+  display: flex;
+`;
 export default AvatarGroup;
