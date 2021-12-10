4b6394fc73ab44864a3ef75eb5ac1a68584752b8
jinn2u <bsybear623@gmail.com>
2021-12-10 23:30:45 +0900

    :sparkles:  feature: kakaoAuth페이지 리다이렉트하기

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,6 +1,10 @@
 import { Container, Wrapper, Text, Img, Button } from './style';
 import LoginWallPaper from '../../assets/LoginPage/LoginWallPaper.jpeg';
+
 const LogInPage = () => {
+  const handleBtnClick = () => {
+    location.href = process.env.REACT_APP_REDIRECT_URI as string;
+  };
   return (
     <Wrapper>
       <Container>
@@ -8,7 +12,7 @@ const LogInPage = () => {
           당신의 순간을 <br /> 그대로 남겨보세요
         </Text>
         <Img src={LoginWallPaper} alt="로그인 페이지 이미지" />
-        <Button>카카오로 3초만에 시작하기</Button>
+        <Button onClick={handleBtnClick}>카카오로 3초만에 시작하기</Button>
       </Container>
     </Wrapper>
   );
