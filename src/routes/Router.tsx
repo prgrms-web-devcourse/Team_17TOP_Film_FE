3c2c6c4e7668705c1b91fde832673591099abcb9
jinn2u <bsybear623@gmail.com>
2021-12-12 16:54:45 +0900

    :sparkles:  feature: 토큰 받는 페이지, 회원가입 페이지 라우팅 처리

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
-import { LogInPage, HomePage, CreatePostPage } from '../pages';
+import { LogInPage, HomePage, OauthPage, CreatePostPage, SignUpPage } from '../pages';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/*" element={<HomePage />} />
-      <Route path="login" element={<LogInPage />} />
-      <Route path="post/create" element={<CreatePostPage />} />
+      <Route path="/oauth/redirect/*" element={<OauthPage />} />
+      <Route path="/login" element={<LogInPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/post/create" element={<CreatePostPage />} />
     </Routes>
   );
 };
