ee3aa9db5f2cabb2121bddfafcda13a171933dd3
jinn2u <bsybear623@gmail.com>
2021-12-12 17:27:09 +0900

    :sparkles:  feature: 토큰을 받아와 라우팅하기
    회원가입을 하지 않았다면 닉네임 입력 페이지로 전환되며, 회원가입이 완료된 유저라면 홈으로 이동합니다.

--- src/pages/OauthPage/index.tsx
+++ src/pages/OauthPage/index.tsx
@@ -1,19 +1,26 @@
-import { useEffect } from 'react';
+import React, { useCallback, useLayoutEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import qs from 'qs';
+import { isUserSignUpApi } from '../../utils/apis/user';
 
 const OauthPage = () => {
   const test = useLocation();
   const navigate = useNavigate();
+  const routingBasedOnSignUpStatus = useCallback(async (token: string) => {
+    const isSignUp = await isUserSignUpApi(token as string);
 
-  const { token } = qs.parse(test.search, {
-    ignoreQueryPrefix: true,
-  });
-
-  localStorage.setItem('token', JSON.stringify(token));
-
-  useEffect(() => {
+    if (isSignUp) {
+      return navigate('/');
+    }
     navigate('/signup');
+  }, []);
+
+  useLayoutEffect(() => {
+    const { token } = qs.parse(test.search, {
+      ignoreQueryPrefix: true,
+    });
+    localStorage.setItem('token', JSON.stringify(token));
+    routingBasedOnSignUpStatus(token as string);
   }, []);
   return <></>;
 };
