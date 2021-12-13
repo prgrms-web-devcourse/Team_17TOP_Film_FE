b30469506bc8b01025c302762d09d7a9aa1e30bd
jinn2u <bsybear623@gmail.com>
2021-12-13 22:49:01 +0900

    :sparkles:  feature: axios interceptor response type 지정하기

---
+++ src/utils/apis/config/type.ts
@@ -0,0 +1,25 @@
+import { AxiosInstance, AxiosInterceptorManager, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+export type CustomResponseFormat<T = any> = {
+  response: T;
+};
+export interface CustomInstance extends AxiosInstance {
+  interceptors: {
+    request: AxiosInterceptorManager<AxiosRequestConfig>;
+    response: AxiosInterceptorManager<AxiosResponse<CustomResponseFormat>>;
+  };
+  getUri(config?: AxiosRequestConfig): string;
+  request<T>(config: AxiosRequestConfig): Promise<T>;
+  get<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+  delete<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+  head<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+  options<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+  post<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+  put<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+  patch<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+}
+
+export interface ErrorType {
+  code: string;
+  message?: string;
+}
