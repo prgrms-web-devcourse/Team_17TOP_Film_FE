222484f41df9e5de21722c8652f74e581ea3b067
jinn2u <bsybear623@gmail.com>
2021-12-13 19:04:33 +0900

    :pencil2:  fix: 인터페이스명을 카멜케이스에서 파스칼케이스로 변경

--- src/utils/apis/config/types.ts
+++ src/utils/apis/config/types.ts
@@ -1,20 +1,20 @@
 import { AxiosInstance, AxiosInterceptorManager, AxiosRequestConfig, AxiosResponse } from 'axios';
 
-interface customAxiosRequestConfig extends AxiosRequestConfig {
+interface CustomAxiosRequestConfig extends AxiosRequestConfig {
   token: string;
 }
 export interface CustomInstance extends AxiosInstance {
   interceptors: {
-    request: AxiosInterceptorManager<customAxiosRequestConfig>;
+    request: AxiosInterceptorManager<CustomAxiosRequestConfig>;
     response: AxiosInterceptorManager<AxiosResponse<any>>;
   };
-  getUri(config?: customAxiosRequestConfig): string;
-  request<T>(config: customAxiosRequestConfig): Promise<T>;
-  get<T>(url: string, config?: customAxiosRequestConfig): Promise<T>;
-  delete<T>(url: string, config?: customAxiosRequestConfig): Promise<T>;
-  head<T>(url: string, config?: customAxiosRequestConfig): Promise<T>;
-  options<T>(url: string, config?: customAxiosRequestConfig): Promise<T>;
-  post<T>(url: string, data?: any, config?: customAxiosRequestConfig): Promise<T>;
+  getUri(config?: CustomAxiosRequestConfig): string;
+  request<T>(config: CustomAxiosRequestConfig): Promise<T>;
+  get<T>(url: string, config?: CustomAxiosRequestConfig): Promise<T>;
+  delete<T>(url: string, config?: CustomAxiosRequestConfig): Promise<T>;
+  head<T>(url: string, config?: CustomAxiosRequestConfig): Promise<T>;
+  options<T>(url: string, config?: CustomAxiosRequestConfig): Promise<T>;
+  post<T>(url: string, data?: any, config?: CustomAxiosRequestConfig): Promise<T>;
   put<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
-  patch<T>(url: string, data?: any, config?: customAxiosRequestConfig): Promise<T>;
+  patch<T>(url: string, data?: any, config?: CustomAxiosRequestConfig): Promise<T>;
 }
