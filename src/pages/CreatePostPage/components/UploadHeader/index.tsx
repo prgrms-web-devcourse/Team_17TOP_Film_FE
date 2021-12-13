82ed466b3d5dbab2cbf0997f1e044bd006b8311b
jinn2u <bsybear623@gmail.com>
2021-12-13 15:30:23 +0900

    :sparkles:  feat: 왼쪽에 들어갈 버튼 컴포넌트 정의

--- src/pages/CreatePostPage/components/UploadHeader/index.tsx
+++ src/pages/CreatePostPage/components/UploadHeader/index.tsx
@@ -1,12 +1,11 @@
 import React, { MouseEventHandler } from 'react';
 import { CustomHeader } from './style';
-
 interface Props {
   handleBackBtn: MouseEventHandler<HTMLElement>;
 }
 
 const UploadHeader = ({ handleBackBtn }: Props) => {
-  return <CustomHeader handleLeftEvent={handleBackBtn} midText="필름 만들기" />;
+  return <CustomHeader leftComp="backBtn" handleLeftEvent={handleBackBtn} midText="필름 만들기" />;
 };
 
 export default UploadHeader;
