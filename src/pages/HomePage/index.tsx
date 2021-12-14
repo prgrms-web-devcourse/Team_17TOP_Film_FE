798539fa4b6fd4dbf5708aa0abc5580e05a8a3b3
이소정 <2sjeong918@gmail.com>
2021-12-14 23:45:07 +0900

    :sparkles:  feat: postList api 연동

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,37 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import { PreviewBottomSheet } from '../../components/organism';
 import { Button, Modal } from '../../components/atoms';
 import { HomePageHeader, PostCreateBtn, ModalWrapper, ButtonGroup, ModalText } from './style';
 import Map from './Map';
 import { Cookies } from 'react-cookie';
+import { getPostListApi } from '../../utils/apis/post';
+import { Post, PreviewPost } from '../../utils/apis/post/type';
 
-const dummy = [
-  {
-    postId: 0,
-    state: 'Closed',
-    location: {
-      latitude: '37.491837217869616',
-      longitude: '127.02959879978368',
-    },
-  },
-  {
-    postId: 1,
-    state: 'Opend',
-    location: {
-      latitude: '37.48802955953209',
-      longitude: '127.02570733476914',
-    },
-  },
-  {
-    postId: 2,
-    state: 'Openable',
-    location: {
-      latitude: '37.48722960663839',
-      longitude: '127.0299415713133',
-    },
-  },
-];
 const PreviewPosts = [
   {
     postId: 0,
@@ -107,41 +83,23 @@ const PreviewPosts = [
   },
 ];
 
-interface Post {
-  postId: number;
-  state: string;
-  location: {
-    latitude: string;
-    longitude: string;
-  };
-}
-interface PreviewPost {
-  postId: number;
-  title: string;
-  previewText: string;
-  availableAt: string;
-  state: string;
-  location: {
-    latitude: string;
-    longitude: string;
-  };
-  authorityCount: number;
-  authorityImageList: {
-    imageOrder: number;
-    authorityId: number;
-    imageUrl: string;
-  }[];
-}
-
 const HomePage = () => {
   const cookies = new Cookies();
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
   const [isMap, setIsMap] = useState(false);
+  const [postList, setPostList] = useState<Post[]>([]);
   const [selectedPost, setselectedPost] = useState<PreviewPost | null>(null);
   const [openablePosts, setOpenablePosts] = useState<Post[] | null>(null);
   const [modalVisible, setModalVisible] = useState(false);
+
+  const getPostList = useCallback(async () => {
+    const { data, error } = await getPostListApi();
+    if (data) {
+      setPostList((prev) => [...prev, ...data.posts]);
+    }
+  }, [getPostListApi]);
 
   const handleSelectedPost = (postid: number) => {
     // 엿보기 페이지 api 통신
@@ -167,12 +125,13 @@ const HomePage = () => {
   };
 
   useEffect(() => {
-    // Map 리스트 api 통신
+    getPostList();
+
     if (cookies.get('invisibleModal')) {
       setIsMap(true);
       return;
     }
-    const currentOpenablePosts = dummy.filter((post) => post.state === 'Openable');
+    const currentOpenablePosts = postList.filter((post) => post.state === 'Openable');
     if (currentOpenablePosts.length) {
       setOpenablePosts(currentOpenablePosts);
       setModalVisible(true);
@@ -192,7 +151,7 @@ const HomePage = () => {
         <Map
           currentLocation={!pathname.slice(1) ? true : false}
           selectedPost={selectedPost}
-          postList={dummy}
+          postList={postList}
           onClick={handleSelectedPost}
         />
       )}
