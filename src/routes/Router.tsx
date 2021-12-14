55e67de8b0d1135ed6fec645c759e290d5b5065a
jinn2u <bsybear623@gmail.com>
2021-12-15 05:59:11 +0900

    :sparkles:  feat: 마이페이지 라우팅

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+
 import {
   LogInPage,
   HomePage,
   OauthPage,
   CreatePostPage,
   SignUpPage,
+  MyPage,
   PostDetailPage,
 } from '../pages';
 import PrivateRoute from './PrivateRoute';
@@ -16,12 +18,12 @@ const Router = () => {
         <Route path="/login" element={<LogInPage />} />
         <Route path="/oauth/redirect/*" element={<OauthPage />} />
       </Route>
-
       <Route element={<PrivateRoute />}>
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/*" element={<HomePage />} />
         <Route path="/post/create" element={<CreatePostPage />} />
         <Route path="/post/:postId" element={<PostDetailPage />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Route>
     </Routes>
   );
