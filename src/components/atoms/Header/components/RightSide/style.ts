b8e77e33f4f50709461c18d61059bda16fe398ef
jinn2u <bsybear623@gmail.com>
2021-12-15 05:48:48 +0900

    :pencil2:  fix: 헤더의 컴포넌트 포인터 커서가 되도록 변경

--- src/components/atoms/Header/components/RightSide/style.ts
+++ src/components/atoms/Header/components/RightSide/style.ts
@@ -1,9 +1,8 @@
 import styled from '@emotion/styled';
 
-const Wrapper = styled.button`
-  position: absolute;
-  padding: 0;
-  right: ${({ theme }) => theme.gaps.default_margin};
+const Wrapper = styled.div`
+  margin: 0 28px 0 auto;
+  cursor: pointer;
 `;
 
 export { Wrapper };
