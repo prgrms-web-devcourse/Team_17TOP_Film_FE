17b3c8fed7baa17a3f8c58539024d8db1aae5b4b
jinn2u <bsybear623@gmail.com>
2021-12-14 17:18:56 +0900

    :sparkles:  feature: Private Route설정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import { LogInPage, HomePage, OauthPage, CreatePostPage, SignUpPage } from '../pages';
+import PrivateRoute from './PrivateRoute';
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/*" element={<HomePage />} />
-      <Route path="/oauth/redirect/*" element={<OauthPage />} />
       <Route path="/login" element={<LogInPage />} />
+      <Route path="/oauth/redirect/*" element={<OauthPage />} />
       <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/post/create" element={<CreatePostPage />} />
+      <Route element={<PrivateRoute />}>
+        <Route path="/*" element={<HomePage />} />
+        <Route path="/post/create" element={<CreatePostPage />} />
+      </Route>
     </Routes>
   );
 };
