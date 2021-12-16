f617e52d99358dff1d6c3bc20eeabd170220044f
이소정 <2sjeong918@gmail.com>
2021-12-16 17:00:14 +0900

    :sparkles:  feat: PostDetailPage 헤더 추가 및 버튼 라우팅 처리

--- src/pages/PostDetailPage/index.tsx
+++ src/pages/PostDetailPage/index.tsx
@@ -1,5 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+import { Header } from '../../components/atoms';
+
 const PostDetailPage = () => {
-  return <div>PostDetailPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <Header leftComp="backBtn" handleLeftEvent={() => navigate(-1)} midText="사진 보기" />
+    </div>
+  );
 };
 
 export default PostDetailPage;
