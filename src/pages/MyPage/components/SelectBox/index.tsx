4d2d8c247513343c0ae0ca6dd6638760c9c53ba8
jinn2u <bsybear623@gmail.com>
2021-12-18 14:32:40 +0900

    :lipstick:  style: selectBox를 눌렀을 때 option의 디자인

--- src/pages/MyPage/components/SelectBox/index.tsx
+++ src/pages/MyPage/components/SelectBox/index.tsx
@@ -1,22 +1,46 @@
-import { Dispatch, SetStateAction, useRef } from 'react';
-import { Select, Label } from './style';
+import { Dispatch, MouseEvent, SetStateAction, useEffect, useRef } from 'react';
+import { Select, Label, SelectItemWrapper, SelectItem } from './style';
+import useSelect from './useSelect';
 
 interface Props {
   options: string[];
   selectedOption: string;
   setSelectedOption: Dispatch<SetStateAction<string>>;
   width: string | number;
+  maxHeight: number;
 }
-const SelectBox = ({ options, selectedOption, setSelectedOption, width }: Props) => {
+
+const SelectBox = ({ options, selectedOption, setSelectedOption, width, maxHeight }: Props) => {
+  const labelRef = useRef<HTMLLabelElement>(null);
+  const [clickSelectedBox, setClickSelectedBox] = useSelect(labelRef);
+
+  const handleOpenSelectBox = (e: MouseEvent) => {
+    e.preventDefault();
+    setClickSelectedBox(true);
+  };
+  const handleSelectBox = (e: any, option?: string) => {
+    option ? setSelectedOption(option) : setSelectedOption(e.target.value);
+    setClickSelectedBox(false);
+  };
+
   return (
-    <Label width={width}>
-      <Select value={selectedOption}>
+    <Label width={width} ref={labelRef} onMouseDown={handleOpenSelectBox}>
+      <Select value={selectedOption} onChange={handleSelectBox}>
         {options.map((option) => (
           <option key={option} value={option}>
             {option}
           </option>
         ))}
       </Select>
+      {clickSelectedBox && (
+        <SelectItemWrapper maxHeight={maxHeight}>
+          {options.map((option) => (
+            <SelectItem key={option} onClick={(e) => handleSelectBox(e, option)}>
+              {option}
+            </SelectItem>
+          ))}
+        </SelectItemWrapper>
+      )}
     </Label>
   );
 };
--- src/pages/MyPage/components/SelectBox/style.ts
+++ src/pages/MyPage/components/SelectBox/style.ts
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-import { RiArrowDownSFill } from 'react-icons/ri';
 
 const Label = styled.label<{ width: number | string }>`
   position: relative;
@@ -30,4 +29,37 @@ const Select = styled.select`
   -moz-appearance: none;
 `;
 
-export { Select, Label };
+const SelectItemWrapper = styled.ul<{ maxHeight: number }>`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  list-style: none;
+  overflow-y: scroll;
+  background-color: white;
+  z-index: 100;
+  border: 1px solid black;
+  border-radius: 4px;
+  max-height: ${({ maxHeight }) => `${maxHeight}px`};
+  & {
+    ::-webkit-scrollbar {
+      width: 10px;
+    }
+    ::-webkit-scrollbar-track {
+      border-radius: 4px;
+    }
+    ::-webkit-scrollbar-thumb {
+      border-radius: 4px;
+      background: ${({ theme: { colors } }) => colors.primary};
+    }
+  }
+`;
+const SelectItem = styled.li`
+  padding: 10px 10px;
+  cursor: pointer;
+  background-color: white;
+
+  &:hover {
+    background-color: ${({ theme: { colors } }) => colors.gray200};
+  }
+`;
+export { Select, Label, SelectItemWrapper, SelectItem };
