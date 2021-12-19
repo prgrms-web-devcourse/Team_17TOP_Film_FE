ced46cab8733baefad067e48e868a505d6baef6c
김정호 <gkood2@gmail.com>
2021-12-20 00:21:57 +0900

    :bug:  bugfix: 검색결과에 본인 포함되지않도록 수정

--- src/components/organism/SearchUser/index.tsx
+++ src/components/organism/SearchUser/index.tsx
@@ -4,6 +4,7 @@ import ResultList from './ResultList';
 import { useSelectedUserList } from '../../../contexts/SelectedUserListProvider';
 import { getSearchUser } from '../../../utils/apis/searchUser';
 import Toast from '../Toast';
+import { useUserInfo } from '../../../contexts/UserProvider';
 
 export interface UserInfo {
   nickname: string;
@@ -18,6 +19,7 @@ const SearchUser = () => {
   const [divLoaded, setDivLoaded] = useState(false);
   const selectedUserList = useSelectedUserList();
   const [lastNickname, setLastNickname] = useState('');
+  const userInfo = useUserInfo();
 
   const handleSearchInput = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setSearchKeyword(e.target.value);
@@ -39,9 +41,12 @@ const SearchUser = () => {
       Toast.warn('서버에 문제가 있네요 잠시후에 다시 시도 해주세요');
       return;
     }
-    if (userData.data.length < 1) return;
-    setLastNickname(userData.data[userData.data.length - 1].nickname);
-    firstSend ? setUserList(userData.data) : setUserList((prev) => [...prev, ...userData.data]);
+    const searchResults = userData.data.filter(
+      (user) => user.nickname !== userInfo.userInfo.nickname,
+    );
+    if (searchResults.length < 1) return;
+    setLastNickname(searchResults[searchResults.length - 1].nickname);
+    firstSend ? setUserList(searchResults) : setUserList((prev) => [...prev, ...searchResults]);
   };
 
   useEffect(() => {
