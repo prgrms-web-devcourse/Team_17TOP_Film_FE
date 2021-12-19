fb8969d53f555f8675c3064cd4560b1b2ab3cdea
jinn2u <bsybear623@gmail.com>
2021-12-19 18:02:20 +0900

    :pencil2:  fix: 헤더의 컴포넌트들을 button 타입으로 수정 && svg icon 통일

--- src/components/atoms/Header/components/RightSide/index.tsx
+++ src/components/atoms/Header/components/RightSide/index.tsx
@@ -1,10 +1,7 @@
 import React, { MouseEvent } from 'react';
 import { BiLogOut, BiTrash, BiUser } from 'react-icons/bi';
-
-import { BsLockFill } from 'react-icons/bs';
 import { Text } from '../../..';
 import { Wrapper } from './style';
-
 export interface Props {
   rightComp?: 'lock' | 'timeline' | 'logout' | 'delete' | 'mypage';
   handleRightEvent?: (e: MouseEvent<HTMLElement>) => void;
@@ -13,7 +10,6 @@ export interface Props {
 const RightSide = ({ rightComp, handleRightEvent }: Props) => {
   return (
     <Wrapper onClick={handleRightEvent}>
-      {rightComp === 'lock' && <BsLockFill />}
       {rightComp === 'timeline' && <Text textType="Paragraph2">타임라인</Text>}
       {rightComp === 'logout' && <BiLogOut size={24} />}
       {rightComp === 'delete' && <BiTrash size={24} />}
