af3aba2c0be40d05401295f9d69115105d3e1775
jinn2u <bsybear623@gmail.com>
2021-12-19 17:47:04 +0900

    :sparkles:  feat: 아바타에 들어갈 속성을 만들어주는 함수
    image가 없을 경우 default Image를 넣어주도록 함

---
+++ src/pages/MyPage/util/createAvatarList.ts
@@ -0,0 +1,7 @@
+import img_profile from '../../../assets/images/img_profile.svg';
+
+const createAvatarList = ({ imageOrder, imageUrl }: { imageOrder: number; imageUrl?: string }) => ({
+  src: imageUrl ? imageUrl : img_profile,
+  alt: `프로필 이미지${imageOrder}`,
+});
+export default createAvatarList;
