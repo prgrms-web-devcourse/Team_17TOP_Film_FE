81c33a8246fb53748edf5d203f82d1805a9f7f6f
jinn2u <bsybear623@gmail.com>
2021-12-19 17:48:00 +0900

    :fire:  remove: console.log 제거

--- src/pages/MyPage/util/findValueOfSelectList.ts
+++ src/pages/MyPage/util/findValueOfSelectList.ts
@@ -1,7 +1,6 @@
 import { selectList, tabList, TabListKey } from '../constants';
 
 const findValueOfSelectList = (selectedTab: TabListKey) => {
-  console.log(selectedTab, 'asd');
   const key = Object.entries(tabList).find(([_, value]) => value === selectedTab);
   if (!key) return [''];
   return Object.values(selectList[key[0] as TabListKey]);
