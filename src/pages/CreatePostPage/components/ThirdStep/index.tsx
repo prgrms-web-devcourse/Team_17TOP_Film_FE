b608c9b2db9511bf537ba60b19e68b653e6219a6
김정호 <gkood2@gmail.com>
2021-12-20 16:01:04 +0900

    :bug:  bugfix:minDay 계산시 localeDate(new Date())가 아닌 KST 사용하도록 구현

--- src/pages/CreatePostPage/components/ThirdStep/index.tsx
+++ src/pages/CreatePostPage/components/ThirdStep/index.tsx
@@ -45,10 +45,19 @@ const ThirdStep = ({
   };
 
   const dateValidate = (date: string) => {
-    const today = new Date();
-    const tomorrow = new Date(today.setDate(today.getDate() + 2)).getDate();
+    const tomorrow = getKST(true).getDate();
     const storedDate = new Date(date).getDate();
     return tomorrow > storedDate ? false : true;
+  };
+
+  const getKST = (tomorrow: boolean) => {
+    const curr = new Date();
+    const utc = curr.getTime() + curr.getTimezoneOffset() * 60 * 1000;
+    const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+    const TOMORROW_TIME_DIFF = 24 * 60 * 60 * 1000;
+    const kr_curr = new Date(utc + KR_TIME_DIFF);
+    const kr_tomorrow = new Date(utc + KR_TIME_DIFF + TOMORROW_TIME_DIFF);
+    return tomorrow ? kr_tomorrow : kr_curr;
   };
 
   useEffect(() => {
@@ -71,9 +80,7 @@ const ThirdStep = ({
       setMinDay(date);
       return;
     }
-    const today = new Date();
-    const tomorrow = new Date(today.setDate(today.getDate() + 2)).toISOString().split('T')[0];
-
+    const tomorrow = getKST(true).toISOString().split('T')[0];
     setMinDay(tomorrow);
     setDate(tomorrow);
   }, []);
