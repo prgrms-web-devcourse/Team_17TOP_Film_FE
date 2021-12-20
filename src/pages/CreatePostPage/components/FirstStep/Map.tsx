69753915c47e913a40ed9595c31a26220acda56d
김정호 <gkood2@gmail.com>
2021-12-20 22:05:36 +0900

    :sparkles:  feat: 마커이동 클릭으로 가능하도록 구현, 마커 크기 키움

--- src/pages/CreatePostPage/components/FirstStep/Map.tsx
+++ src/pages/CreatePostPage/components/FirstStep/Map.tsx
@@ -1,5 +1,5 @@
-import { useCallback, useEffect, useState } from 'react';
-import MapGl, { Marker } from 'react-map-gl';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import MapGl, { MapEvent, MapRef, Marker } from 'react-map-gl';
 import { MAP_STYLE } from '../../../../utils/constants/mapConstants';
 import Pin from './Pin';
 import { Location } from '../../types';
@@ -17,6 +17,7 @@ const Map = ({ latitude, longitude, marker, onChangeMarker }: Props) => {
     longitude: 126,
     zoom: 15,
   });
+  const mapRef = useRef<any>();
 
   useEffect(() => {
     setViewport({
@@ -37,8 +38,17 @@ const Map = ({ latitude, longitude, marker, onChangeMarker }: Props) => {
       longitude: event.lngLat[0],
     });
   }, []);
+
+  const handleOnClickMap = (e: MapEvent) => {
+    onChangeMarker({
+      latitude: e.lngLat[1],
+      longitude: e.lngLat[0],
+    });
+  };
   return (
     <MapGl
+      ref={mapRef}
+      onClick={handleOnClickMap}
       {...viewport}
       width="100%"
       height="100%"
@@ -51,8 +61,10 @@ const Map = ({ latitude, longitude, marker, onChangeMarker }: Props) => {
         longitude={marker.longitude}
         draggable
         onDragEnd={onMarkerDragEnd}
+        offsetTop={-60}
+        offsetLeft={-20}
       >
-        <Pin size={30}></Pin>
+        <Pin size={60}></Pin>
       </Marker>
     </MapGl>
   );
