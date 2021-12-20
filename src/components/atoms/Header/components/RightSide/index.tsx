82ee1a7db4f09535788dc7561ce1c18181ec05d1
이소정 <2sjeong918@gmail.com>
2021-12-20 23:21:05 +0900

    :lipstick:  style: 헤더 프로필 이미지 반영

--- src/components/atoms/Header/components/RightSide/index.tsx
+++ src/components/atoms/Header/components/RightSide/index.tsx
@@ -1,19 +1,28 @@
 import React, { MouseEvent } from 'react';
 import { BiLogOut, BiTrash, BiUser } from 'react-icons/bi';
-import { Text } from '../../..';
+import { Avatar, Text } from '../../..';
 import { Wrapper } from './style';
+import ProfileImg from '../../../../../assets/images/img_profile.svg';
+import { useUserInfo } from '../../../../../contexts/UserProvider';
 export interface Props {
   rightComp?: 'timeline' | 'logout' | 'delete' | 'mypage';
   handleRightEvent?: (e: MouseEvent<HTMLElement>) => void;
 }
 
 const RightSide = ({ rightComp, handleRightEvent }: Props) => {
+  const { userInfo } = useUserInfo();
   return (
     <Wrapper onClick={handleRightEvent}>
       {rightComp === 'timeline' && <Text textType="Paragraph2">타임라인</Text>}
       {rightComp === 'logout' && <BiLogOut size={24} />}
       {rightComp === 'delete' && <BiTrash size={24} />}
-      {rightComp === 'mypage' && <BiUser size={24} />}
+      {rightComp === 'mypage' && (
+        <Avatar
+          src={userInfo.profileImageUrl ? userInfo.profileImageUrl : ProfileImg}
+          size={26}
+          alt="profile"
+        />
+      )}
     </Wrapper>
   );
 };
