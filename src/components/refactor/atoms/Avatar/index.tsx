54bb163b2f168d5bd8cae73463f2973918daba20
jinn2u <bsybear623@gmail.com>
2022-01-11 17:26:15 +0900

    :recycle:  refactor: click 이벤트 추가하기

--- src/components/refactor/atoms/Avatar/index.tsx
+++ src/components/refactor/atoms/Avatar/index.tsx
@@ -1,14 +1,16 @@
-import AvatarGroup from './AvatarGroup';
 import { VALID_AVATAR } from './constants';
 import Img, { Props as ImgProps } from '../Img';
+import { MouseEvent } from 'react';
 
 interface Props extends Omit<ImgProps, 'height' | 'width' | 'borderRadius' | 'block'> {
   size: number;
+  onClick?: (event: MouseEvent<HTMLImageElement>) => void;
 }
 
-const Avatar = ({ size, lazy, threshold, src, alt, placeholder, ...props }: Props) => {
+const Avatar = ({ size, lazy, threshold, src, alt, placeholder, onClick, ...props }: Props) => {
   return (
     <Img
+      onClick={onClick}
       {...props}
       lazy={lazy}
       threshold={threshold}
@@ -21,10 +23,7 @@ const Avatar = ({ size, lazy, threshold, src, alt, placeholder, ...props }: Prop
     />
   );
 };
-
 Avatar.defaultProps = {
   __TYPE: VALID_AVATAR,
 };
-
-Avatar.Group = AvatarGroup;
 export default Avatar;
