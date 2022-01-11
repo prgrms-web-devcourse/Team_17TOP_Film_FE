7bbed01e3eaca2c87779a0c89d5f062bd258a6a1
jinn2u <bsybear623@gmail.com>
2022-01-11 16:12:47 +0900

    :recycle:  refactor: 기존 Avatar 컴포넌트 추가

---
+++ src/components/refactor/atoms/Avatar/AvatarGroup.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement, ReactNode } from 'react';
+import { VALID_AVATAR } from './constants';
+import { AvatarGroupWrapper } from './style';
+
+interface Props {
+  children: ReactNode;
+  overlapPx?: number;
+}
+
+const AvatarGroup = ({ children, overlapPx = 0 }: Props) => {
+  const avatars = React.Children.toArray(children)
+    .filter((element: ReactNode) => {
+      if (React.isValidElement(element) && element.props.__TYPE === VALID_AVATAR) {
+        return true;
+      }
+      return false;
+    })
+    .map((avatar, index, avatars) => {
+      const item = avatar as ReactElement;
+      return React.cloneElement(item, {
+        ...item.props,
+        style: {
+          marginLeft: `${index === 0 ? 0 : -overlapPx}px`,
+          zIndex: avatars.length - index,
+        },
+      });
+    });
+  return <AvatarGroupWrapper>{avatars}</AvatarGroupWrapper>;
+};
+
+export default AvatarGroup;
