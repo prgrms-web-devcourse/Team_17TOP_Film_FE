339618caa1ba7fcfbc815c7a7a22f2eb652e087c
김정호 <gkood2@gmail.com>
2022-01-13 17:12:41 +0900

    :sparkles:  feat: 속성별 storybook 작성

--- src/stories/components/Input.stories.tsx
+++ src/stories/components/Input.stories.tsx
@@ -10,7 +10,6 @@ export default {
     },
     placeholder: {
       type: { name: 'string' },
-      defaultValue: 'Placeholder',
       control: { type: 'text' },
     },
     invalid: {
@@ -34,4 +33,39 @@ export default {
 
 export const Default = (args: any) => {
   return <Input {...args}></Input>;
+};
+export const Type = (args: any) => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+      <Input type={'text'} {...args}></Input>
+      <Input type={'date'} {...args}></Input>
+    </div>
+  );
+};
+export const Placeholder = (args: any) => {
+  return <Input placeholder={'Placeholder'} {...args}></Input>;
+};
+export const Invalid = (args: any) => {
+  return <Input invalid {...args}></Input>;
+};
+export const Disabled = (args: any) => {
+  return <Input disabled {...args}></Input>;
+};
+export const Width = (args: any) => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+      <Input {...args}></Input>
+      <Input width={'50%'} {...args}></Input>
+      <Input width={300} {...args}></Input>
+    </div>
+  );
+};
+export const BorderRadius = (args: any) => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+      <Input {...args}></Input>
+      <Input borderRadius={'50%'} {...args}></Input>
+      <Input borderRadius={10} {...args}></Input>
+    </div>
+  );
 };
