14e1b24ba2589113b9e666de3d9e6246a5382e00
jinn2u <bsybear623@gmail.com>
2022-01-20 19:19:42 +0900

    :wrench:  config: 절대경로를 사용하기 위한 설정

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,17 @@
+import CracoAlias from 'craco-alias';
+
 module.exports = {
   babel: {
     presets: ['@emotion/babel-preset-css-prop'],
   },
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './src',
+        tsConfigPath: './tsconfig.paths.json',
+      },
+    },
+  ],
 };
