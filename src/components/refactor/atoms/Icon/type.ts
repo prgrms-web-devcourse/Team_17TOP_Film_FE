ab2a40ab0fec3c4da0d8d5c6173af6d16e04545d
이소정 <2sjeong918@gmail.com>
2022-01-25 15:28:11 +0900

    :triangular_flag_on_post:  test: storybook을 위한 아이콘 타입 추가

--- src/components/refactor/atoms/Icon/type.ts
+++ src/components/refactor/atoms/Icon/type.ts
@@ -2,6 +2,7 @@ import { SVGProps } from 'react';
 import * as icons from '../../../../assets/icons';
 
 type IconType = keyof typeof icons;
+export const iconTypes: IconType[] = Object.keys(icons) as any[];
 
 export interface IconProps extends SVGProps<SVGSVGElement> {
   icon: IconType;
