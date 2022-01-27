54dc34c62b6926ef0a07bd26ff85139ace0d4786
이소정 <2sjeong918@gmail.com>
2022-01-27 16:48:44 +0900

    :pencil2:  fix: Icon 컴포넌트 size 속성 string인 경우 size 그대로 받을 수 있도록 수정

--- src/components/refactor/atoms/Icon/style.ts
+++ src/components/refactor/atoms/Icon/style.ts
@@ -3,11 +3,11 @@ import styled from '@emotion/styled';
 
 const IconStyle = styled.span<Pick<IconProps, 'color' | 'size'>>`
   display: inline-block;
-  width: ${({ size }) => (typeof size === 'number' ? `${size}px` : '')};
-  height: ${({ size }) => (typeof size === 'number' ? `${size}px` : '')};
+  width: ${({ size }) => (typeof size === 'number' ? `${size}px` : size)};
+  height: ${({ size }) => (typeof size === 'number' ? `${size}px` : size)};
   svg {
-    width: ${({ size }) => (typeof size === 'number' ? `${size}px` : '')};
-    height: ${({ size }) => (typeof size === 'number' ? `${size}px` : '')};
+    width: ${({ size }) => (typeof size === 'number' ? `${size}px` : size)};
+    height: ${({ size }) => (typeof size === 'number' ? `${size}px` : size)};
   }
   path {
     fill: ${({ color }) => (color ? color : '')};
