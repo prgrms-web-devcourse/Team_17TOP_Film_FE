409414a397b6949ecac728cddd3b01596c195749
김정호 <gkood2@gmail.com>
2022-01-28 16:50:53 +0900

    :recycle:  refactor: Loader 컴포넌트  style.ts에 Text import 절대경로로 수정

--- src/components/refactor/organism/Loader/style.ts
+++ src/components/refactor/organism/Loader/style.ts
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { Text } from '../../atoms';
+import { Text } from '@components/atoms';
 const LoaderWrapper = styled.div`
   position: fixed;
   top: 0;
