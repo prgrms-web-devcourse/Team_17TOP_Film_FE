e716bf1d53391ce0d8e0bd7a94feb0849faba507
김정호 <gkood2@gmail.com>
2022-02-05 15:39:53 +0900

    :sparkles:  feat: Loader 스토리북 작성

---
+++ src/stories/components/Loader.stories.tsx
@@ -0,0 +1,10 @@
+import { Loader } from '../../components/refactor/organism';
+
+export default {
+  title: 'Example/Loader',
+  component: Loader,
+};
+
+export const Default = () => {
+  return <Loader>테스트 로더</Loader>;
+};
