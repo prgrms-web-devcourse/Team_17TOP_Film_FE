e41bfd0d855f44b3b40d9e81ca345a9676f92622
김정호 <gkood2@gmail.com>
2022-02-07 18:04:38 +0900

    :sparkles:  feat: LottieLoader storybook 작성

---
+++ src/stories/components/LottieLoader.stories.tsx
@@ -0,0 +1,32 @@
+import { LottieLoader } from '@refactors/organism';
+import fireworks from '@assets/lotties/fireworks.json';
+import loader from '@assets/lotties/loader.json';
+
+export default {
+  title: 'Example/LottieLoader',
+  component: LottieLoader,
+};
+
+export const Loading = (args: any) => {
+  return (
+    <div style={{ width: '500px', height: '450px', background: 'gray' }}>
+      <LottieLoader
+        {...args}
+        width={500}
+        height={450}
+        options={{ loop: true, animationData: loader, autoplay: true }}
+      ></LottieLoader>
+    </div>
+  );
+};
+
+export const fireWorkEffect = (args: any) => {
+  return (
+    <LottieLoader
+      {...args}
+      width={500}
+      height={450}
+      options={{ loop: true, animationData: fireworks, autoplay: true }}
+    ></LottieLoader>
+  );
+};
