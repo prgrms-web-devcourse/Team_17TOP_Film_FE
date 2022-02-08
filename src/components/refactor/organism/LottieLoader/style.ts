43c545b42773b645fb1fdda2fa356f78d344a123
김정호 <gkood2@gmail.com>
2022-02-08 16:03:35 +0900

    :lipstick:  style: 중심을 기준으로 rotate 되도록 tranform-origin:center로 수정

--- src/components/refactor/organism/LottieLoader/style.ts
+++ src/components/refactor/organism/LottieLoader/style.ts
@@ -5,6 +5,7 @@ const LottieContainer = styled.div<Omit<LottieProps, 'options'>>`
   width: ${({ width }) => (typeof width === 'number' ? `${width}px` : width)};
   height: ${({ height }) => (typeof height === 'number' ? `${height}px` : height)};
   transform: ${({ deg }) => `rotate(${deg}deg)`};
+  transform-origin: center;
 `;
 
 export { LottieContainer };
