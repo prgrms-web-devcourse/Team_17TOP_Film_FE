709b60c3c2a3fe60e5f65c7361615d375c34fa19
김정호 <gkood2@gmail.com>
2022-02-11 17:01:41 +0900

    :pencil2:  fix: width, height type optional로 수정, 기본값 100%로 지

--- src/components/refactor/atoms/LottieLoader/index.tsx
+++ src/components/refactor/atoms/LottieLoader/index.tsx
@@ -3,7 +3,7 @@ import lottie from 'lottie-web';
 import { LottieContainer } from './style';
 import { LottieProps } from './types';
 
-const LottieLoader = ({ width, height, options, deg = 0 }: LottieProps) => {
+const LottieLoader = ({ width = '100%', height = '100%', options, deg = 0 }: LottieProps) => {
   const lottieContainer = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (lottieContainer.current) {
--- src/components/refactor/atoms/LottieLoader/types.ts
+++ src/components/refactor/atoms/LottieLoader/types.ts
@@ -1,8 +1,8 @@
 import { AnimationConfigWithData, AnimationConfigWithPath } from 'lottie-web';
 
 export interface LottieProps {
-  width: string | number;
-  height: string | number;
+  width?: string | number;
+  height?: string | number;
   options: Omit<AnimationConfigWithPath, 'container'> | Omit<AnimationConfigWithData, 'container'>;
   deg?: number;
 }
