e29ac33b9cec668a4ddea1803f897123fbef4f22
jinn2u <bsybear623@gmail.com>
2022-02-11 18:29:43 +0900

    :recycle:  refactor: BottomNavigation이항상 밑에 가도록 수정

--- src/components/refactor/organism/BottomNavigation/style.ts
+++ src/components/refactor/organism/BottomNavigation/style.ts
@@ -1,13 +1,14 @@
 import styled from '@emotion/styled';
 
 const Wrapper = styled.nav<{ bgColor: string }>`
-  position: sticky;
+  position: fixed;
   width: 100%;
   height: 52px;
   bottom: 0;
   left: 0;
   background-color: ${({ bgColor }) => bgColor};
   box-shadow: 2px -7px 8px 0px rgba(0, 0, 0, 0.1);
+  z-index: 900;
 `;
 
 const Container = styled.ul`
