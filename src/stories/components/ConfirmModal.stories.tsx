f15c54522f397eda2b6ed4fb3926e2da23719623
이소정 <2sjeong918@gmail.com>
2022-02-14 17:51:59 +0900

    :triangular_flag_on_post:  test: ConfirmModal 스토리북 추가

---
+++ src/stories/components/ConfirmModal.stories.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { ConfirmModal } from '@refactors/organism';
+import { Button } from '@refactors/atoms';
+
+export default {
+  title: 'Example/ConfirmModal',
+  component: ConfirmModal,
+  argTypes: {
+    modalText: {
+      type: { name: 'string', required: true },
+      control: { type: 'text' },
+    },
+    primaryBtnText: {
+      type: { name: 'string', required: true },
+      control: { type: 'text' },
+    },
+    secondaryBtnText: {
+      type: { name: 'string', required: true },
+      control: { type: 'text' },
+    },
+    primaryBtnEvent: {
+      action: '확인 클릭',
+    },
+    secondaryBtnEvent: {
+      action: '취소 클릭',
+    },
+  },
+};
+
+export const Default = (args: any) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <div>
+      <Button btnStyle="primary" size="medium" onClick={() => setModalVisible(true)}>
+        ConfirmModal 열기
+      </Button>
+      {modalVisible && (
+        <ConfirmModal
+          modalText={`모달 텍스트를 넣어주세요`}
+          primaryBtnText={`확인`}
+          secondaryBtnText={`취소`}
+          dimClose={() => setModalVisible(false)}
+          primaryBtnEvent={() => {
+            console.log('확인 클릭');
+          }}
+          secondaryBtnEvent={() => {
+            console.log('취소 클릭');
+          }}
+          {...args}
+        />
+      )}
+    </div>
+  );
+};
