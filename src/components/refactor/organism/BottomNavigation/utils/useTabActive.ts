9c206fb46e92cadc32377768fa9fc026639697af
jinn2u <bsybear623@gmail.com>
2022-02-24 22:34:21 +0900

    :recycle:  refactor: 현재 활성화되어있는 탭의 색을 보여주는 useTabClick함수

---
+++ src/components/refactor/organism/BottomNavigation/utils/useTabActive.ts
@@ -0,0 +1,25 @@
+import {
+  Dispatch,
+  ReactChild,
+  ReactElement,
+  ReactFragment,
+  ReactNode,
+  ReactPortal,
+  SetStateAction,
+  useState,
+} from 'react';
+
+export const useIsTabActive = (
+  children: ReactChild[],
+): [number, Dispatch<SetStateAction<number>>] => {
+  const [isTabActive, setIsTabActive] = useState(() => {
+    const activeTabs = children.map((element, idx) => {
+      if ((element as ReactElement).props.active) {
+        return idx;
+      }
+    });
+    const activeIdx = activeTabs.find((v) => v !== undefined);
+    return activeIdx ? activeIdx : 0;
+  });
+  return [isTabActive, setIsTabActive];
+};
