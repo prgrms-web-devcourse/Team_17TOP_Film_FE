04b0fbee8423e1bfa71bc14fc9ff21a8887d8632
jinn2u <bsybear623@gmail.com>
2022-02-27 11:19:53 +0900

    :recycle:  refactor: 직관적이게 함수 이름과 파일명 변경

--- src/components/refactor/organism/BottomNavigation/BNItem/findDeviceType.ts
+++ src/components/refactor/organism/BottomNavigation/BNItem/findDeviceType.ts
@@ -1,4 +1,4 @@
-export const isMobile = () => {
+export const findDeviceType = () => {
   const device = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
   return device.test(navigator.userAgent);
 };
