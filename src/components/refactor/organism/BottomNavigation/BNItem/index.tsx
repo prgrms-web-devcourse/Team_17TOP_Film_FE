46cbde163f1f0969f61e9a76ea7d68e72d5ef777
jinn2u <bsybear623@gmail.com>
2022-02-27 11:20:57 +0900

    :recycle:  refactor: 바뀐 함수명 적용

--- src/components/refactor/organism/BottomNavigation/BNItem/index.tsx
+++ src/components/refactor/organism/BottomNavigation/BNItem/index.tsx
@@ -2,7 +2,7 @@ import { Li, StyledLink } from './style';
 import * as icons from '@assets/icons';
 import { Icon } from '@refactors/atoms';
 import { VALID_BNITEM } from '../constants';
-import { isMobile } from './utils';
+import { findDeviceType } from './findDeviceType';
 
 interface Props {
   text?: string;
@@ -14,10 +14,11 @@ interface Props {
   [x: string]: any;
 }
 const BNItem = ({ text, icon, href, iconSize, direction, ...props }: Props) => {
-  const test = isMobile();
+  const deviceType = findDeviceType();
+
   return (
     <Li {...props}>
-      <StyledLink className={test ? 'mobile' : 'web'} to={href} direction={direction}>
+      <StyledLink className={deviceType ? 'mobile' : 'web'} to={href} direction={direction}>
         {icon && <Icon icon={icon} size={iconSize} />}
         {text}
       </StyledLink>
