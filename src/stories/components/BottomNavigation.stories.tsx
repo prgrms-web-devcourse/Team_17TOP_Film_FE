dcf2a185123862d6f5da847b63bfafe59b593cdf
jinn2u <bsybear623@gmail.com>
2022-02-27 11:24:46 +0900

    :recycle:  refactor: BottomNavigation 스토리북

--- src/stories/components/BottomNavigation.stories.tsx
+++ src/stories/components/BottomNavigation.stories.tsx
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-import { Icon } from '@refactors/atoms';
 import { BNItem, BottomNavigation } from '@refactors/organism';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
@@ -16,96 +15,113 @@ export default {
     dividerColor: {
       control: { type: 'text' },
     },
-    itemHoverColor: {
-      control: { type: 'text' },
-    },
   },
 } as ComponentMeta<typeof BottomNavigation>;
 
 export const Default: ComponentStory<typeof BottomNavigation> = (args) => {
   return (
-    <BottomNavigation {...args}>
-      <BNItem href="/asdf" icon="markerOpen" iconSize={40} text="footer1" />
-      <BNItem href="/asdf" icon="markerClose" iconSize={40} text="footer1" />
-    </BottomNavigation>
+    <Wrapper>
+      <CuBtnNav {...args}>
+        <BNItem href="/asdf" icon="markerOpen" text="footer1" />
+        <BNItem href="/asdf" icon="markerClose" text="footer1" />
+      </CuBtnNav>
+    </Wrapper>
   );
 };
-
 export const ChildrenWrong: ComponentStory<typeof BottomNavigation> = (args) => {
-  return <BottomNavigation {...args}>asdf</BottomNavigation>;
+  return (
+    <Wrapper>
+      <CuBtnNav {...args}>asdf</CuBtnNav>
+    </Wrapper>
+  );
 };
 
 export const NoIcon: ComponentStory<typeof BottomNavigation> = (args) => {
   return (
-    <BottomNavigation {...args}>
-      <BNItem href="/asdf" text="footer1" />
-      <BNItem href="/asdf" text="footer1" />
-    </BottomNavigation>
+    <Wrapper>
+      <CuBtnNav {...args}>
+        <BNItem href="/asdf" text="footer1" />
+        <BNItem href="/asdf" text="footer1" />
+      </CuBtnNav>
+    </Wrapper>
   );
 };
 
 export const IconSizeUp: ComponentStory<typeof BottomNavigation> = (args) => {
   return (
-    <BottomNavigation {...args}>
-      <BNItem href="/asdf" icon="markerOpen" iconSize={40} text="footer1" />
-      <BNItem href="/asdf" icon="markerClose" iconSize={40} text="footer1" />
-    </BottomNavigation>
+    <Wrapper>
+      <CuBtnNav {...args}>
+        <BNItem href="/asdf" icon="markerOpen" iconSize={50} text="footer1" />
+        <BNItem href="/asdf" icon="markerClose" iconSize={50} text="footer1" />
+      </CuBtnNav>
+    </Wrapper>
   );
 };
 export const DisplayColumn: ComponentStory<typeof BottomNavigation> = (args) => {
   return (
-    <BottomNavigation direction="column" {...args}>
-      <BNItem
-        style={{ background: 'red' }}
-        href="/asdf"
-        icon="markerOpen"
-        iconSize={40}
-        text="footer1"
-      />
-      <BNItem href="/asdf" icon="markerClose" iconSize={40} text="footer1" />
-    </BottomNavigation>
+    <Wrapper>
+      <CuBtnNav direction="column" {...args}>
+        <BNItem href="/asdf" icon="markerOpen" iconSize={40} text="footer1" />
+        <BNItem href="/asdf" icon="markerClose" iconSize={40} text="footer1" />
+      </CuBtnNav>
+    </Wrapper>
   );
 };
 
 export const hasDivider: ComponentStory<typeof BottomNavigation> = (args) => {
   return (
-    <BottomNavigation direction="column" divider {...args}>
-      <BNItem
-        style={{ background: 'red' }}
-        href="/asdf"
-        icon="markerOpen"
-        iconSize={40}
-        text="footer1"
-      />
-      <BNItem href="/asdf" icon="markerClose" iconSize={40} text="footer1" />
-    </BottomNavigation>
+    <Wrapper>
+      <CuBtnNav divider {...args}>
+        <BNItem href="/asdf" icon="markerOpen" iconSize={40} text="footer1" />
+        <BNItem href="/asdf" icon="markerClose" iconSize={40} text="footer1" />
+      </CuBtnNav>
+    </Wrapper>
+  );
+};
+export const dividerColorRed: ComponentStory<typeof BottomNavigation> = (args) => {
+  return (
+    <Wrapper>
+      <CuBtnNav divider dividerColor="red" {...args}>
+        <BNItem href="/asdf" icon="markerOpen" iconSize={40} text="footer1" />
+        <BNItem href="/asdf" icon="markerClose" iconSize={40} text="footer1" />
+      </CuBtnNav>
+    </Wrapper>
   );
 };
 export const DividerColorChange: ComponentStory<typeof BottomNavigation> = (args) => {
   return (
-    <BottomNavigation direction="column" divider dividerColor="red" {...args}>
-      <BNItem
-        style={{ background: 'red' }}
-        href="/asdf"
-        icon="markerOpen"
-        iconSize={40}
-        text="footer1"
-      />
-      <BNItem href="/asdf" icon="markerClose" iconSize={40} text="footer1" />
-    </BottomNavigation>
+    <Wrapper>
+      <CuBtnNav divider dividerColor="red" {...args}>
+        <BNItem href="/asdf" icon="markerOpen" iconSize={40} text="footer1" />
+        <BNItem href="/asdf" icon="markerClose" iconSize={40} text="footer1" />
+      </CuBtnNav>
+    </Wrapper>
   );
 };
-export const Hover: ComponentStory<typeof BottomNavigation> = (args) => {
+export const DefaultActive: ComponentStory<typeof BottomNavigation> = (args) => {
   return (
-    <BottomNavigation direction="column" divider dividerColor="red" itemHoverColor="red" {...args}>
-      <BNItem
-        style={{ background: 'red' }}
-        href="/asdf"
-        icon="markerOpen"
-        iconSize={40}
-        text="footer1"
-      />
-      <BNItem href="/asdf" icon="markerClose" iconSize={40} text="footer1" />
-    </BottomNavigation>
+    <Wrapper>
+      <CuBtnNav activeColor="red" {...args}>
+        <BNItem href="/asdf" icon="markerOpen" iconSize={40} text="footer1" />
+        <BNItem href="/asdf" icon="markerClose" iconSize={40} text="footer1" />
+      </CuBtnNav>
+    </Wrapper>
   );
 };
+export const ActiveSetting: ComponentStory<typeof BottomNavigation> = (args) => {
+  return (
+    <Wrapper>
+      <CuBtnNav activeColor="red" {...args}>
+        <BNItem href="/asdf" icon="markerOpen" iconSize={40} text="footer1" />
+        <BNItem active href="/asdf" icon="markerClose" iconSize={40} text="footer1" />
+      </CuBtnNav>
+    </Wrapper>
+  );
+};
+const CuBtnNav = styled(BottomNavigation)`
+  position: absolute;
+`;
+const Wrapper = styled.div`
+  position: relative;
+  height: 100px;
+`;
