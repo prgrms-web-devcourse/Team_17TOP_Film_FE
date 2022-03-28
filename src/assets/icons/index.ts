1d946551b3035713bb87d9a4d7a323683b89eb0d
이소정 <2sjeong918@gmail.com>
2022-03-28 23:14:48 +0900

    :recycle:  refactor: 사용중인 아이콘 정리

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,4 +3,5 @@ export { ReactComponent as markerClose } from './icon_marker_close.svg';
 export { ReactComponent as markerCloseSelected } from './icon_marker_close_selected.svg';
 export { ReactComponent as markerOpen } from './icon_marker_open.svg';
 export { ReactComponent as markerOpenSelected } from './icon_marker_open_selected.svg';
-export { BiChevronLeft } from 'react-icons/bi';
+export { BiChevronLeft, BiLogOut, BiTrash, BiX, BiLeftArrowAlt } from 'react-icons/bi';
+export { MdHome, MdCollections, MdPerson, MdClose, MdErrorOutline } from 'react-icons/md';
